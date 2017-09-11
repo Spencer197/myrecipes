8e5477e8ab5f475a5b4e7f9118af3bf68683ab03
Spencer197 <spncr@protonmail.com>
2017-09-11 11:53:42 +0000

    Complete homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,28 @@ class PagesController < ApplicationController
   def home
     
   end
+  
+  def about
+    
+  end
+  
+  def help
+    
+  end
+  
+  def chefs
+    
+  end
+  
+  def chat
+    
+  end
+  
+  def ingredience
+    
+  end
+  
+  def recipes
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get 'pages/home', to: 'pages#home' 
+  
+  root "pages#home"#Root goes to pages controller, home action.
+  get '/about', to: 'pages#about'#Get the '/about' route and send it to the Pages controller, about action.
+  get '/help', to: 'pages#help'#Get the '/help' route and send it to the pages controller, help action.
+  get '/chefs', to: 'pages#chefs'
+  get '/chat', to: 'pages#chat'
+  get '/ingredients', to: 'pages#ingredients'
+  get '/recipes', to: 'pages#recipes'
+  #root "pages#home"
+  #get 'pages/home', to: 'pages#home' 
+
+resources :myrecipes
 end
