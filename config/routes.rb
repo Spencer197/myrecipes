17dbaa031401456cb7daa540d006729ecbd185ac
Spencer197 <spncr@protonmail.com>
2017-10-26 14:08:24 +0000

    Add ingredient listing, show page, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/help', to: 'pages#help'
   get '/chat', to: 'pages#chat'
-  get '/ingredients', to: 'pages#ingredients'
+  #get '/ingredients', to: 'pages#ingredients'
   #get 'recipes', to: 'recipes#index'
   #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'# "as: 'new_recipe'" adds "new_recipe(_path)" to the prefix.
   #get '/recipes/:id', to: 'recipes#show', as: 'recipe'# "as: 'recipe'" adds  "recipe(_path)" to the prefix.
   #post '/recipes', to: 'recipes#create'
-  #The above commented routes are all replaced by the line below.
+  #The above commented routes are all replaced by the lines below.
   resources :recipes
   
   get '/signup', to: 'chefs#new'#This line directs the '/signup' route to the chef controller, new action rather than the new route.
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
   
   get '/login', to: 'sessions#new'#Goes to sessions controller, new action.
   post '/login', to: 'sessions#create'#This will submit/post the login new session form to the sessions controller, create action.
-  delete '/logout', to: 'sessions#destroy'#This goes down the logout path to the sessions controller, destroy action. 
+  delete '/logout', to: 'sessions#destroy'#This goes down the logout path to the sessions controller, destroy action.
+  
+  resources :ingredients, except: [:destroy]
 end
